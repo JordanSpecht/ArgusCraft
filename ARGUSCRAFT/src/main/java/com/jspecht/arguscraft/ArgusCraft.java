9b180cedb00a00bd1318bec142232e269459fc2d
Jordan Specht <jspecht@iai.pri>
2019-09-30 22:30:32 -0400

    added gateway

--- ARGUSCRAFT/src/main/java/com/jspecht/arguscraft/ArgusCraft.java
+++ ARGUSCRAFT/src/main/java/com/jspecht/arguscraft/ArgusCraft.java
@@ -4,20 +4,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.jspecht.arguscraft.blocks.argus_pole;
+import com.jspecht.arguscraft.blocks.gateway;
 import com.jspecht.arguscraft.lists.BlockList;
 import com.jspecht.arguscraft.lists.ItemList;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.*;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.tileentity.LecternTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -59,7 +65,9 @@ public class ArgusCraft {
 					ItemList.antenna = new Item(new Item.Properties().group(argus)).setRegistryName(location("antenna")),
 					ItemList.solarpanel = new Item(new Item.Properties().group(argus)).setRegistryName(location("solarpanel")),
 					
-					ItemList.argus_pole = new BlockItem(BlockList.argus_pole, new Item.Properties().group(argus)).setRegistryName("argus_pole")
+					//Block Items
+					ItemList.argus_pole = new BlockItem(BlockList.argus_pole, new Item.Properties().group(argus)).setRegistryName("argus_pole"),
+					ItemList.gateway = new BlockItem(BlockList.gateway, new Item.Properties().group(argus)).setRegistryName("gateway")
 			);
 			
 			logger.info("Items registered.");
@@ -69,11 +77,12 @@ public class ArgusCraft {
 		public static void registerBlocks(final RegistryEvent.Register<Block> event) {
 			
 			event.getRegistry().registerAll(
-					BlockList.argus_pole = new argus_pole(Block.Properties.create(Material.WOOD).hardnessAndResistance(2.0f, 3.0f).lightValue(0).sound(SoundType.SCAFFOLDING))
-			);
+					BlockList.argus_pole = new argus_pole(Block.Properties.create(Material.WOOD).hardnessAndResistance(2.0f, 3.0f).lightValue(0).sound(SoundType.SCAFFOLDING)),
+					BlockList.gateway = new gateway(Block.Properties.create(Material.GLASS).hardnessAndResistance(2.0f, 3.0f).lightValue(2).sound(SoundType.METAL))
+					);
 			
 			logger.info("Blocks registered.");
-		}
+		}		
 		
 		private static ResourceLocation location(String name) {
 			return new ResourceLocation(modid, name);
